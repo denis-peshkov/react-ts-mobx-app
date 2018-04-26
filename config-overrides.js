f51fe42643f3e79910fd9fc0338133428f1238cd
Denis Peshkov <peshkov@bk.ru>
2018-04-26 15:05:36 +0300

    add mobx

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,12 @@
-const {injectBabelPlugin} = require('react-app-rewired');
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireMobX = require('react-app-rewire-mobx');
 
 module.exports = function override(config, env) {
+  // add a plugin
+  config = injectBabelPlugin('babel-plugin-styled-components', config);
 
-  
+  // use the MobX rewire
+  config = rewireMobX(config, env);
+
   return config
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
+import { observable } from 'mobx';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+
+
+class Timer {
+  @observable public start = Date.now();
+}
+
+const t = new Timer();
+module.exports = {t};
 
 ReactDOM.render(
   <App />,
